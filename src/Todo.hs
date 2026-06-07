5e6f265138a2f293d51b9726a404265a197f1799
Marco Kuoni <mail@marcokuoni.ch>
2026-06-07 12:33:23 +0200

    changing name convention

--- src/Todo.hs
+++ src/Todo.hs
@@ -1,47 +1,51 @@
-module Todo
-  ( Status(..)
-  , Task(..)
-  , TodoList
-  , addTask
-  , completeTask
-  , removeTask
-  , pendingTasks
-  , completedTasks
-  ) where
+module Todo (
+    Status (..),
+    Task (..),
+    TodoList,
+    addTask,
+    completeTask,
+    removeTask,
+    pendingTasks,
+    completedTasks,
+) where
 
 -- | A task can either still be Pending or already Done.
 data Status = Pending | Done deriving (Eq, Show)
 
 -- | A single task: a unique numeric id, a textual description, and a status.
 data Task = Task
-  { taskId      :: Int
-  , description :: String
-  , status      :: Status
-  } deriving (Eq, Show)
+    { taskId :: Int
+    , description :: String
+    , status :: Status
+    }
+    deriving (Eq, Show)
 
 -- | A todo list is just a list of tasks.
 type TodoList = [Task]
 
--- | Add a new task to the list with status Pending.
---   The new id must be (maximum existing id) + 1, or 1 if the list is empty.
---   The new task should be appended at the end.
+{- | Add a new task to the list with status Pending.
+  The new id must be (maximum existing id) + 1, or 1 if the list is empty.
+  The new task should be appended at the end.
+-}
 addTask :: String -> TodoList -> TodoList
-addTask _desc _tasks = undefined
+addTask desc tasks = undefined
 
--- | Mark the task with the given id as Done.
---   If no task has that id, return the list unchanged.
+{- | Mark the task with the given id as Done.
+  If no task has that id, return the list unchanged.
+-}
 completeTask :: Int -> TodoList -> TodoList
-completeTask _tid _tasks = undefined
+completeTask tid tasks = undefined
 
--- | Remove the task with the given id.
---   If no task has that id, return the list unchanged.
+{- | Remove the task with the given id.
+  If no task has that id, return the list unchanged.
+-}
 removeTask :: Int -> TodoList -> TodoList
-removeTask _tid _tasks = undefined
+removeTask tid tasks = undefined
 
 -- | All tasks whose status is Pending (order preserved).
 pendingTasks :: TodoList -> [Task]
-pendingTasks _tasks = undefined
+pendingTasks tasks = undefined
 
 -- | All tasks whose status is Done (order preserved).
 completedTasks :: TodoList -> [Task]
-completedTasks _tasks = undefined
+completedTasks tasks = undefined
